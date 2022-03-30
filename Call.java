65dfcfc6b98442c4af0ec31a72f8cccab80c04ba
Saiteja_MI <54614106+SaitejaMI@users.noreply.github.com>
2022-03-30 13:45:38 +0530

    Add files via upload

--- Call.java
+++ Call.java
@@ -1,187 +1,53 @@
-package com.example.calllogs;
+package com.example.calllogs.Objects;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-import android.Manifest;
-import android.app.Activity;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Bundle;
-import android.provider.CallLog;
-import android.view.View;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
-import com.example.calllogs.Objects.Call;
-import com.example.calllogs.Objects.Contact;
-
-
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity {
+public class Call {
+    private int duration, type;
+    private long date;
+    private String number, name;
 
-    TextView t1;
-    ArrayList<Call> calls = new ArrayList<>();
-    ArrayList<Contact> contacts = new ArrayList<>();
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CALL_LOG}, PackageManager.PERMISSION_GRANTED);
+    public Call(){
 
     }
 
-
-    public void getCall(View view) {
-
-        String output = "";
-
-//        getting info of call logs
-        Uri uricall = Uri.parse("content:/call_log/calls");
-
-//           getting info of call logs
-        Cursor cursorcall = getContentResolver().query(CallLog.Calls.CONTENT_URI, null, null, null, null);
-
-        String number;
-        String name;
-        int callDuration;
-        String dateString;
-        int dir=-1;
-        int index=-1;
-        String type=null;
-
-        cursorcall.moveToFirst();
-
-        do {
-
-            Call call = new Call();
-            Contact contact = new Contact();
-
-            number = cursorcall.getString(cursorcall.getColumnIndex(CallLog.Calls.NUMBER));
-            call.setNumber(number);
-
-            name = cursorcall.getString(cursorcall.getColumnIndex(CallLog.Calls.CACHED_NAME));
-            if(name != null)
-                call.setName(name);
-            else
-                call.setName(number);
-
-            callDuration = Integer.parseInt(cursorcall.getString(cursorcall.getColumnIndex(CallLog.Calls.DURATION)));
-            call.setDuration(callDuration);
-
-            int dates = cursorcall.getColumnIndex(CallLog.Calls.DATE);
-            String callDate = cursorcall.getString(dates);
-            long callDayTime = Long.parseLong(callDate);
-            call.setDate(callDayTime);
-
-            String callType = cursorcall.getString(cursorcall.getColumnIndex(CallLog.Calls.TYPE));
-            int dircode = Integer.parseInt(callType);
-            switch (dircode) {
-                case CallLog.Calls.OUTGOING_TYPE:
-                    dir = 0;
-                    break;
-                    case CallLog.Calls.INCOMING_TYPE:
-                    dir = 1;
-                    break;
-                case CallLog.Calls.MISSED_TYPE:
-                    dir = 2;
-                    break;
-            }
-            call.setType(dir);
-
-
-            calls.add(call);
-
-
-            index = -1;
-            for(int i =0;i<contacts.size();i++){
-                    if(number.equals(contacts.get(i).getNumber())) {
-                        index = i;
-
-                        break;
-                    }
-                }
-
-            if(index == -1) {
-                contact.setNumber(call.getNumber());
-                contact.setName(call.getName());
-                contacts.add(contact);
-                index = contacts.size()-1;
-            }
-
-            switch (dir){
-                case 0:
-                    contacts.get(index).setOutgoingDuration(contacts.get(index).getOutgoingDuration()+callDuration);
-                    contacts.get(index).setNumberOfOutgoingCalls(contacts.get(index).getNumberOfOutgoingCalls()+1);
-                    contacts.get(index).setLongestDuration(Math.max(contacts.get(index).getLongestDuration(),callDuration));
-                    break;
-                case 1:
-                    contacts.get(index).setIncomingDuration(contacts.get(index).getIncomingDuration()+callDuration);
-                    contacts.get(index).setNumberOfIncomingCalls(contacts.get(index).getNumberOfIncomingCalls()+1);
-                    contacts.get(index).setLongestDuration(Math.max(contacts.get(index).getLongestDuration(),callDuration));
-                case 2:
-                    contacts.get(index).setNumberOfMissedCalls(contacts.get(index).getNumberOfMissedCalls()+1);
-            }
-
-
-
-
-               /*output += "Number  :  " + number + "\n \n" +
-                        "Name  :  " + name + "\n \n" +
-                        "Duration  :  " + callDuration + "\n \n" +
-                        "Date  :  " + dateString + "\n \n" +
-                        "Type  :  " + callType(dir) + "\n \n" +
-                        "-----------------------------------------------" + "\n";*/
-
-
-        }
-            while (cursorcall.moveToNext());
-            for(int i =0;i< calls.size();i++) {
-                output = output+calls.get(i).getName() + " " +calls.get(i).getNumber() +"\n \n"+
-                        calls.get(i).getDuration()+ " " + dateString(calls.get(i).getDate())+" "+callType(calls.get(i).getType())+"\n \n" ;
-            }
-
-        for(int i = 0; i < contacts.size();i++){
-            output = output+ contacts.get(i).getName() + " "+ contacts.get(i).getNumber()+"\n \n"
-            + contacts.get(i).getNumberOfOutgoingCalls()+" "+contacts.get(i).getOutgoingDuration()+"\n \n"
-            + contacts.get(i).getNumberOfIncomingCalls()+" "+contacts.get(i).getIncomingDuration()+"\n \n"
-            + contacts.get(i).getNumberOfTotalCalls()+" "+contacts.get(i).getLongestDuration()+"\n \n"
-            + contacts.get(i).getNumberOfMissedCalls()+" "+ contacts.get(i).getTotalDuration()+"\n \n"
-            +"-----------------------------------------------" + "\n"
-            ;
-        }
-            t1.setText(output);
-
+    public String getNumber(){
+        return number;
     }
 
-    public String callType(int num){
-        if(num == 0){
-            return "Outgoing";
-        }else if (num == 1){
-            return "Incoming";
-        }else if(num == 2){
-            return "missed";
-        }else {
-            return "other";
-        }
+    public void setNumber(String number){
+        this.number = number;
     }
 
-    public String dateString(long callDayTime){
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
-        return formatter.format(callDayTime);
+    public void setName(String name){
+        this.name = name;
     }
 
-    public void sort_function(){
-
+    public String getName(){
+        return name;
     }
- }
+
+    public int getType(){
+        return type;
+    }
+
+    public void setType(int type){
+        this.type = type;
+    }
+
+    public long getDate(){
+        return date;
+    }
+
+    public void setDate(long date){
+        this.date = date;
+    }
+
+    public int getDuration(){
+        return duration;
+    }
+
+    public void setDuration(int duration){
+        this.duration = duration;
+    }
+}
---
+++ Contact.java
@@ -0,0 +1,50 @@
+package com.example.calllogs.Objects;
+
+public class Contact {
+    private String name,number;
+    private int incomingDuration=0, outgoingDuration=0, totalDuration=0, longestDuration=0;
+    private int numberOfOutgoingCalls=0, numberOfIncomingCalls=0, numberOfTotalCalls=0, numberOfMissedCalls=0;
+    private int averageDuration=0, numberOfAverageCalls=0;
+
+    public Contact(){}
+
+    public String getNumber(){
+        return number;
+    }
+    public void setNumber(String number){
+        this.number = number;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public void setIncomingDuration(int incomingDuration){ this.incomingDuration = incomingDuration;}
+    public int getIncomingDuration(){ return incomingDuration;}
+
+    public void setOutgoingDuration(int outgoingDuration){ this.outgoingDuration = outgoingDuration;}
+    public int getOutgoingDuration(){ return outgoingDuration;}
+
+    public int getTotalDuration(){ return incomingDuration+outgoingDuration;}
+
+    public void setLongestDuration(int longestDuration){ this.longestDuration = longestDuration;}
+    public int getLongestDuration(){ return longestDuration;}
+
+    public void setNumberOfOutgoingCalls(int numberOfOutgoingCalls){ this.numberOfOutgoingCalls = numberOfOutgoingCalls;}
+    public int getNumberOfOutgoingCalls(){ return numberOfOutgoingCalls;}
+
+    public void setNumberOfIncomingCalls(int numberOfIncomingCalls){ this.numberOfIncomingCalls = numberOfIncomingCalls;}
+    public int getNumberOfIncomingCalls(){ return numberOfIncomingCalls;}
+
+    public int getNumberOfTotalCalls(){ return numberOfIncomingCalls+numberOfMissedCalls+numberOfOutgoingCalls;}
+
+    public void setNumberOfMissedCalls(int numberOfMissedCalls){ this.numberOfMissedCalls = numberOfMissedCalls;}
+    public int getNumberOfMissedCalls(){ return numberOfMissedCalls;}
+
+    public int getAverageDuration(){ return totalDuration/numberOfTotalCalls;}
+
+    public int getNumberOfAverageCalls(int days){ return numberOfTotalCalls/days;}
+}
